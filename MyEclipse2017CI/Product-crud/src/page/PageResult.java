83ecd3423b85b34ea0165b3909264a5b50160d6b
zj_jian9394 <736070323@qq.com>
2018-11-26 09:42:28 +0800

    SaaS

--- MyEclipse2017CI/Product-crud/src/page/PageResult.java
+++ MyEclipse2017CI/Product-crud/src/page/PageResult.java
@@ -5,8 +5,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.ToString;
 @SuppressWarnings("all")
-@Getter
-@ToString
+
 public class PageResult {
 	private List listData;
 	private Integer totalCount;
@@ -26,6 +25,37 @@ public class PageResult {
 		this.totalPage = this.totalCount%this.PageSize == 0 ?this.totalCount/this.PageSize:this.totalCount/this.PageSize+1;
 		this.prevPage = this.currentPage -1 >=1?this.currentPage-1 :1;
 		this.nextPage = this.currentPage +1 <=this.totalPage?this.currentPage+1 :this.totalPage;
+	}
+	
+	@Override
+	public String toString() {
+		return "PageResult [listData=" + listData + ", totalCount=" + totalCount + ", currentPage=" + currentPage
+				+ ", PageSize=" + PageSize + ", beginPage=" + beginPage + ", totalPage=" + totalPage + ", prevPage="
+				+ prevPage + ", nextPage=" + nextPage + "]";
+	}
+	public List getListData() {
+		return listData;
+	}
+	public Integer getTotalCount() {
+		return totalCount;
+	}
+	public Integer getCurrentPage() {
+		return currentPage;
+	}
+	public Integer getPageSize() {
+		return PageSize;
+	}
+	public Integer getBeginPage() {
+		return beginPage;
+	}
+	public Integer getTotalPage() {
+		return totalPage;
+	}
+	public Integer getPrevPage() {
+		return prevPage;
+	}
+	public Integer getNextPage() {
+		return nextPage;
 	}
 	
 	
